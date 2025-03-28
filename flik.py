169a16e2f9c9154c10a8bd832b9befa4751b6913
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-28 17:31:41 +0900

    bam

--- flik.py
+++ flik.py
@@ -1,11 +1,11 @@
 @lambda fliky : setattr(__import__('builtins'), 'FLIK', fliky())
 class FLIKY:
-    __slots__ = ('__x', )
-
-    def __init__(self):
-        self.__x = 0
-
-    def __getitem__(self, x):
-        print(f"#define LINE_{self.__x}(x) x {'^&'[x.start]} 0b%0{x.step}d" % x.stop)
-        self.__x += 1
-        return self
+	__slots__ = ('__x', )
+	
+	def __init__(self):
+		self.__x = 0
+	
+	def __getitem__(self, x):
+		print(f"#define LINE_{self.__x}(x) x {'^&'[x.start]} 0b%0{x.step}d" % x.stop)
+		self.__x += 1
+		return self
